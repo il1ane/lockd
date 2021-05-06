72d026c6e25d07c94ddccdd01adf05f841af3356
il1ane <iliane.zedadra@icloud.com>
2021-05-06 17:25:01 +0200

    generate password for every parameter combinaison

--- PasswordGenerator/PasswordViewModel.swift
+++ PasswordGenerator/PasswordViewModel.swift
@@ -9,24 +9,189 @@ import Foundation
 
 class PasswordViewModel: ObservableObject {
     
-    @Published var password = ""
+    @Published var generatedPassword = ""
     
-    func generatePassword(lenght: Int, specialCharacters: Bool, uppercase: Bool) -> String {
+    let alphabet: [String] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let special: [String] = ["(",")","{","}","[","]","/","+","*","$",">",".","|","^","?"]
+    let numbersArray: [String] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    
+    func generatePassword(lenght: Int, specialCharacters: Bool, uppercase: Bool, numbers: Bool) -> String {
         
-        let alphabet: [String] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        var uppercasedAlphabet: [String] = []
+        //All parameters
+        if uppercase && specialCharacters && numbers {
+            return threeParameterPassword(lenght: lenght)
+        }
+        
+        //Two parameters
+        if uppercase && specialCharacters || uppercase && numbers || numbers && specialCharacters {
+            return twoParameterPassword(lenght: lenght, specialCharacters: specialCharacters, uppercase: uppercase, numbers: numbers)
+        }
+        
+        //One parameter
+        if uppercase || specialCharacters || numbers {
+            return oneParameterPassword(lenght: lenght, specialCharacters: specialCharacters, uppercase: uppercase, numbers: numbers)
+        }
+        
+        else {
+            return lowercasePassword(lenght: lenght)
+        }
+    }
+    
+    func lowercasePassword(lenght: Int) -> String {
+        
         var password: [String] = [""]
+
+            for _ in 0...lenght {
+                password.append(alphabet.randomElement()!)
+            }
+                return password.joined()
+    }
+    
+    func oneParameterPassword(lenght: Int, specialCharacters: Bool, uppercase: Bool, numbers: Bool) -> String {
         
-        if uppercase {
+        var password: [String] = [""]
+        var uppercasedAlphabet: [String] = []
+        
         for character in alphabet {
             uppercasedAlphabet.append(character.uppercased())
         }
-            for _ in 0...lenght {
-                password.append(uppercasedAlphabet.randomElement()!)
+        
+        if specialCharacters {
+            for _ in 0...lenght / 2 {
+                password.append(alphabet.randomElement()!)
             }
+            
+            for _ in 0...lenght / 2 {
+                password.append(special.randomElement()!)
+            }
+            password.shuffle()
             return password.joined()
         }
         
+        if uppercase {
+            for _ in 0...lenght / 2 {
+                password.append(alphabet.randomElement()!)
+            }
+            
+            for _ in 0...lenght / 2 {
+                password.append(uppercasedAlphabet.randomElement()!)
+            }
+            password.shuffle()
+            return password.joined()
+        }
+        
+        if numbers {
+            for _ in 0...lenght / 2 {
+                password.append(alphabet.randomElement()!)
+            }
+            
+            for _ in 0...lenght / 2 {
+                password.append(numbersArray.randomElement()!)
+            }
+            password.shuffle()
+            return password.joined()
+        }
+        
+        else {
+            return "Error"
+        }
+    }
+    
+    func twoParameterPassword(lenght: Int, specialCharacters: Bool, uppercase: Bool, numbers: Bool) -> String {
+        
+        var password: [String] = [""]
+        var uppercasedAlphabet: [String] = []
+        
+        for character in alphabet {
+            uppercasedAlphabet.append(character.uppercased())
+        }
+        
+        if uppercase && specialCharacters {
+
+            for _ in 0...lenght / 3 {
+                password.append(uppercasedAlphabet.randomElement()!)
+            }
+            
+            for _ in 0...lenght / 3 {
+                password.append(special.randomElement()!)
+            }
+            
+            for _ in 0...lenght / 3 {
+                password.append(alphabet.randomElement()!)
+            }
+            
+            password.shuffle()
+            return password.joined()
+        }
+        
+        if uppercase && numbers {
+
+            for _ in 0...lenght / 3 {
+                password.append(uppercasedAlphabet.randomElement()!)
+            }
+            
+            for _ in 0...lenght / 3 {
+                password.append(numbersArray.randomElement()!)
+            }
+            
+            for _ in 0...lenght / 3 {
+                password.append(alphabet.randomElement()!)
+            }
+            
+            password.shuffle()
+            return password.joined()
+        }
+        
+        if numbers && specialCharacters {
+
+            for _ in 0...lenght / 3 {
+                password.append(numbersArray.randomElement()!)
+            }
+            
+            for _ in 0...lenght / 3 {
+                password.append(special.randomElement()!)
+            }
+            
+            for _ in 0...lenght / 3 {
+                password.append(alphabet.randomElement()!)
+            }
+            
+            password.shuffle()
+            return password.joined()
+        }
+        
+        else {
+            return "Error"
+        }
+       
+    }
+    
+    func threeParameterPassword(lenght: Int) -> String {
+        
+        var password: [String] = [""]
+        var uppercasedAlphabet: [String] = []
+        
+        for character in alphabet {
+            uppercasedAlphabet.append(character.uppercased())
+        }
+        
+        for _ in 0...lenght / 4 {
+            password.append(uppercasedAlphabet.randomElement()!)
+        }
+        
+        for _ in 0...lenght / 4{
+            password.append(special.randomElement()!)
+        }
+        
+        for _ in 0...lenght / 4 {
+            password.append(alphabet.randomElement()!)
+        }
+        
+        for _ in 0...lenght / 4 {
+            password.append(numbersArray.randomElement()!)
+        }
+        
+        password.shuffle()
         return password.joined()
     }
     
