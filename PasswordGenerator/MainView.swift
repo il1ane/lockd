838a40be8d22d58b5e166749919fa000744825f4
il1ane <iliane.zedadra@icloud.com>
2021-05-07 19:58:41 +0200

    add MainView (TabView)

---
+++ PasswordGenerator/MainView.swift
@@ -0,0 +1,30 @@
+//
+//  MainView.swift
+//  PasswordGenerator
+//
+//  Created by Iliane Zedadra on 07/05/2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/,
+                content:  {
+                    PasswordGeneratorView().tabItem { Label(
+                        title: { Text("Générateur") },
+                        icon: { Image(systemName: "rectangle.and.pencil.and.ellipsis") }
+                    ).padding() }.tag(1)
+                    PasswordListView().tabItem { Label(
+                        title: { Text("Coffre fort") },
+                        icon: { Image(systemName: "tray.2") }
+                    ).padding() }.tag(2)
+                })
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
