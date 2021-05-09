8ae59480826124d981326ea1ad92743a4b5d9560
il1ane <iliane.zedadra@icloud.com>
2021-05-09 22:48:16 +0200

    add keychain keys list

--- PasswordGenerator/Store passwords/PasswordListView.swift
+++ PasswordGenerator/Store passwords/PasswordListView.swift
@@ -7,21 +7,41 @@
 
 import SwiftUI
 import Security
+import KeychainSwift
 
 struct PasswordListView: View {
     
-    let passwordsNames = ["Facebook", "Twitter", "Instagram", "Snapchat", "Google", "Apple", "Spotify", "Mail", "Twitch", "League of Legends"]
+    @ObservedObject var viewModel = PasswordListViewModel()
+    @State private var showPasswordView = false
+    @State private var chosenKey = ""
     
     var body: some View {
-         
+        
         NavigationView {
-            List(passwordsNames, id: \.self) { password in
-                Text(password)
-                
-            }.navigationBarTitle("Coffre fort")
-        }
+            
+            
+            List {
+                ForEach(viewModel.keys, id: \.self) { key in
+                    HStack {
+                        Button(action: {
+                                chosenKey = key
+                                showPasswordView.toggle() },
+                               label: Text(key))
+                    }
+                }
+            }.onAppear(perform: {
+                viewModel.refreshKeys()
+            })
+            .navigationBarTitle("Coffre fort")
+            }
+       
+            
+        .sheet(isPresented: $showPasswordView, content: {
+            PasswordView(key: $chosenKey, viewModel: viewModel, isPresented: $showPasswordView)
+        })
     }
 }
+
 
 struct PasswordListView_Previews: PreviewProvider {
     static var previews: some View {
