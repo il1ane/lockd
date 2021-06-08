a598872cad149957d61a96054d1913e89c2e96c4
il1ane <iliane.zedadra@icloud.com>
2021-06-09 00:36:58 +0200

    add tests files

---
+++ PasswordGeneratorTests/PasswordGeneratorTests.swift
@@ -0,0 +1,20 @@
+//
+//  PasswordGeneratorTests.swift
+//  PasswordGeneratorTests
+//
+//  Created by Iliane Zedadra on 08/06/2021.
+//
+
+import XCTest
+@testable import PasswordGenerator
+
+class PasswordGeneratorTests: XCTestCase {
+    
+    func testGeneratePassword() {
+        
+//        let password = PasswordGeneratorViewModel()
+        
+    }
+ 
+
+}
